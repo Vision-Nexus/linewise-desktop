91a8f849b747a10ea588e78802ef6204773dc317
ulgyd0ggy <yibin@linewise.io>
2026-05-10 23:07:39 +0800

    Update ask-linus button layout

--- crates/lw-app/src/icons/mod.rs
+++ crates/lw-app/src/icons/mod.rs
@@ -80,6 +80,43 @@ pub fn GoogleIcon() -> Element {
 }
 
 #[component]
+pub fn ChatIcon() -> Element {
+    rsx! {
+        svg {
+            width: "24",
+            height: "24",
+            view_box: "0 0 24 24",
+            fill: "none",
+            xmlns: "http://www.w3.org/2000/svg",
+            stroke: "currentColor",
+            stroke_width: "2",
+            stroke_linecap: "round",
+            stroke_linejoin: "round",
+            path { d: "M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" }
+        }
+    }
+}
+
+#[component]
+pub fn CloseIcon() -> Element {
+    rsx! {
+        svg {
+            width: "24",
+            height: "24",
+            view_box: "0 0 24 24",
+            fill: "none",
+            xmlns: "http://www.w3.org/2000/svg",
+            stroke: "currentColor",
+            stroke_width: "2",
+            stroke_linecap: "round",
+            stroke_linejoin: "round",
+            line { x1: "18", y1: "6", x2: "6", y2: "18" }
+            line { x1: "6", y1: "6", x2: "18", y2: "18" }
+        }
+    }
+}
+
+#[component]
 pub fn MicrosoftIcon() -> Element {
     rsx! {
         svg {
